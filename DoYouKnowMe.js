5b32c17f9177afb41be4ade86c078c34638c3ea4
SachinBabar17 <sachinbabar1710@gmail.com>
2021-09-26 19:41:03 +0530

    updated

--- DoYouKnowMe.js
+++ DoYouKnowMe.js
@@ -1,39 +1,71 @@
-var readlineSync = require ("readline-sync");
-var score=0;
-
-var userName = readlineSync.question("Whats ur name ?");
-
-console.log("Welcome " +userName+" to Do u know Sachin?");
-console.log("---------------------------------");
+var readlineSync = require('readline-sync');
+var chalk = require('chalk');
+var score = 0;
 
 
 
-function play(que,ans){
-  var userAns = readlineSync.question(que);
+var playerName = readlineSync.question(chalk('Whats your name? \n'));
+console.log(chalk.blue('\n\nHello ',playerName, '...!'));
 
+console.log(chalk.cyan('start the quizzz...\n'));
 
-  if(userAns==ans){
-    console.log("You r Right!");
-    score=score+1;
-  }else{
-    console.log("You r Wrong!");
-    score=score-1;
-  }
-
-  console.log("score: ",score);
-  console.log("------------- ");
-
-
+questionOne = {
+  question:'What is my name? ',
+  answer: 'sachin',
 }
 
-play("Where do I live?","kodaganur");
-play("Where do I study ?","bangalore");
+questionTwo = {
+  question:'where am I from?',
+  answer: 'kodaganur',
+}
+
+questionThree= {
+  question:'Where do I study? ',
+  answer: 'bangalore',
+}
+
+questionFour = {
+  question:'Do i use instragram ? ',
+  answer: 'yes',
+}
+
+questionFive = {
+  question:'What is my favourite food? ',
+  answer: 'chicken',
+}
+
+questionSix= {
+  question:'Whats is my age? ',
+  answer: '20',
+}
 
 
-console.log("------------- ");
-console.log("Yeyh! You scored: ",score);
 
 
+var quizQuestions =[questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix ];
 
+validation(quizQuestions);
+
+function validation(quizQuestions){
+
+  for(let i=0;i<6;i++)
+  {
+    var response = readlineSync.question(chalk(quizQuestions[i].question));
+  
+    if(response.toLocaleLowerCase() == quizQuestions[i].answer){
+      score++;
+      console.log(chalk.bold('CORRECT........  \nyourr score.... '+score));
+    console.log("\n");
+    }
+    else{
+      score--;
+      console.log(chalk.redBright.bold('INCORRECT........  \nyourr score.... '+score));
+    console.log("\n");
+    }
+  
+  }
+  console.log(chalk.bgWhite.black.bold("final score.... "+score));
+  console.log("\n\n\n");
+}
 
 
